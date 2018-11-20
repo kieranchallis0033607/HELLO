89bfb713887de50b95e07a9ee28d773d7e7536a4
Kieran James Challis <rc0033607@student.riversidecollege.ac.uk>
2018-11-20 09:41:50 +0000

    changed hello to goodbye

--- HELLO/Form1.cs
+++ HELLO/Form1.cs
@@ -19,7 +19,7 @@ namespace HELLO
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("HELLO "+txtbxname.Text);
+            MessageBox.Show("goodbye "+txtbxname.Text);
 
         }
     }
